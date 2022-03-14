dbb1a22d89bc73d12b702eea9dc653f8dceddda7
Max <100760716+frameflex@users.noreply.github.com>
2022-03-14 17:01:49 +0100

    Update 1.0.2 | custom_bottom_bar.dart - Button overlap fix

--- lib/view/widgets/custom_bottom_bar.dart
+++ lib/view/widgets/custom_bottom_bar.dart
@@ -35,8 +35,52 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
       child: Row(
         children: [
           SizedBox(
-            width: deviceWidth * .1,
+            width: deviceWidth * 0.1,
           ),
+
+          InkWell(
+            onTap: () {
+              if (widget.selectedIndex == 0) {
+
+              }
+            },
+            child: Container(
+              height: deviceHeight * .1,
+              width: deviceWidth * .2,
+              decoration: BoxDecoration(
+                color:
+                (widget.selectedIndex == 0) ? Colors.black : Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: deviceWidth * .1,
+                    child: Image(
+                      image: (widget.selectedIndex == 0)
+                          ? const AssetImage(
+                        'assets/icons/garage_white_icon.png',
+                      )
+                          : const AssetImage(
+                        'assets/icons/garage_black_icon.png',
+                      ),
+                      fit: BoxFit.scaleDown,
+                    ),
+                  ),
+                  Text(
+                    'Garage',
+                    style: TextStyle(
+                      color: (widget.selectedIndex == 0)
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Spacer(),
+
           InkWell(
             onTap: () {
               if (widget.selectedIndex == 0) {
@@ -52,7 +96,7 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
               }
             },
             child: Container(
-              height: deviceHeight * .08,
+              height: deviceHeight * .1,
               width: deviceWidth * .2,
               decoration: BoxDecoration(
                 color:
@@ -102,7 +146,7 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
               }
             },
             child: Container(
-              height: deviceHeight * .08,
+              height: deviceHeight * .1,
               width: deviceWidth * .2,
               decoration: BoxDecoration(
                 color:
